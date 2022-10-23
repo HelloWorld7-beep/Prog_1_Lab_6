3f3996d4632c7b3eb3ea78dd008e04033b85f6a8
Abigail Lin <77811757+HelloWorld7-beep@users.noreply.github.com>
2022-10-23 17:33:33 -0400

    Update Lab_6_Program.py
    Added subtraction option

--- Lab_6_Program.py
+++ Lab_6_Program.py
@@ -1,4 +1,13 @@
+print("Type 2 numbers")
+
 a = int(input())
 b = int(input())
 
-print(a + b)
+print("If you want to add, type 1. If you want to subtract, type 2")
+
+x = int(input())
+
+if x == 1:
+    print(a + b)
+elif x == 2:
+    print(a - b)
